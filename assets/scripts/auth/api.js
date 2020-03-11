331f038bf8c8d6150de6fe0216e03886480c3f71
rscottlundgren <rscottlundgren@gmail.com>
2020-03-11 15:58:30 -0400

    Add sign-in/sign-up/change-password/sign-out code - 1) Added 'sign-in' code for authentication to the 'api.js' file. 2) Added 'sign-up' code for authentication to the 'api.js' file. 3) Added 'change-password' code for authentication to the 'api.js' file. 4) Added 'sign-out' code for authentication to the 'api.js' file.

---
+++ assets/scripts/auth/api.js
@@ -0,0 +1,52 @@
+'use strict'
+
+const config = require('../config')
+const store = require('../store')
+
+const signUp = function (data) {
+  console.log('In api.js')
+  return $.ajax({
+    url: config.apiUrl + '/sign-up',
+    method: 'POST',
+    data
+  })
+}
+
+const signIn = function (data) {
+  console.log('In api.js')
+  return $.ajax({
+    url: config.apiUrl + '/sign-in',
+    method: 'POST',
+    data
+  })
+}
+
+const changePassword = function (data) {
+  console.log('In api.js')
+  return $.ajax({
+    url: config.apiUrl + '/change-password',
+    method: 'PATCH',
+    headers: {
+      Authorization: 'Token token=' + store.user.token
+    },
+    data
+  })
+}
+
+const signOut = function () {
+  console.log('In api.js')
+  return $.ajax({
+    url: config.apiUrl + '/sign-out',
+    method: 'DELETE',
+    headers: {
+      Authorization: 'Token token=' + store.user.token
+    }
+  })
+}
+
+module.exports = {
+  signUp,
+  signIn,
+  changePassword,
+  signOut
+}
