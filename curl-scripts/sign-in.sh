1222ca929f02fe36a5b5b418b1d51466028f5067
rscottlundgren <rscottlundgren@gmail.com>
2020-03-12 10:17:10 -0400

    Remove Curl-Script Token

--- curl-scripts/sign-in.sh
+++ curl-scripts/sign-in.sh
@@ -4,7 +4,6 @@ curl "https://tic-tac-toe-wdi.herokuapp.com/sign-in" \
   --include \
   --request POST \
   --header "Content-Type: application/json" \
-  --header "Authorization: Token token=${TOKEN}" \
   --data '{
     "credentials": {
       "email": "'"${EMAIL}"'",
