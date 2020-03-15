7f64951d826ed4a2e35c259fdf99b62c2ec5f599
rscottlundgren <rscottlundgren@gmail.com>
2020-03-14 23:22:06 -0400

    Update/Remove Store Object; Add Cell & Game Object - 1) Removed 'player' Key/Value pair from the store object in 'store.js'. 2) Removed 'hash' Key/Value pair from the store object in 'store.js'. 3) Added 'cell' object with 'index' & 'value' Key/Value pairs for easier board updating. 4) Added 'game' object with 'cells' & 'over' key value pairs to track the game board and the over all game status (win/lose/tie). 5) Added comments to the code.

--- assets/scripts/store.js
+++ assets/scripts/store.js
@@ -1,9 +1,34 @@
 'use strict'
 
-const store = {
-  // Player value defaults to "1"
-  player: 1,
-  hash: ['','','','','','','','']
+// Set default value of player to 'X'
+let player = 'X'
+
+// Create variable 'cell' to hold the key/value pairs that will be used for the
+// 'update-game.sh' curl script and ajax call
+const cell = {
+  // Key/Value pair created for the index that was changed in the object by the
+  // player's click that will be needed during ajax call to update the game as
+  // requested
+  index: undefined,
+  // Key/Value pair for the value that was entered into the index created in an
+  // object that will be needed during ajax call to update the game as requested
+  value: ''
 }
 
-module.exports = store
+// Create variable 'game' that will hold the key/value pairs that will be used
+// for both ajax calls and for other features of the game (i.e. determining win,
+// loss, and tie states, etc.)
+const game = {
+  // Key/Value pair that will hold the static status of the entire board
+  // throughout game player
+  cells: ['','','','','','','','',''],
+  // Key/Value pair that will hold the overall 'game state' for determination of
+  // whether the game has been won, lost, or tied
+  over: false
+}
+
+module.exports = {
+  player,
+  cell,
+  game
+}
