9e533b0f6de238570e8722726067590edf61b0bd
rscottlundgren <rscottlundgren@gmail.com>
2020-03-14 23:06:16 -0400

    Add Authorizations, Event Listeners, & Comments - 1) Added authorization code for "gameEngine" (game/engine.js). 2) Added authorization code for "gameEvents" (game/events.js). 3) Added event listener for "Create Game" button. 4) Added event listener that updates the board every tile click. 5) Added comments to code.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,16 +3,29 @@
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example')
 
-const authEvents = require('./auth/events.js')
-const gameApp = require('./game/app.js')
+// Require authorization events file
+const authEvents = require('./auth/events')
+// Require game engine file
+const gameEngine = require('./game/engine')
+// Require game events file
+const gameEvents = require('./game/events')
 
 // use require without a reference to ensure a file is bundled
 // require('./example')
 
 $(() => {
+  // Add event listener for our "Sign Up" form
   $('#sign-up').on('submit', authEvents.onSignUp)
+  // Add event listener for our "Sign In" form
   $('#sign-in').on('submit', authEvents.onSignIn)
+  // Add event listener for our "Change Password" form
   $('#change-password').on('submit', authEvents.onChangePassword)
+  // Add event listener for our "Sign-Out" button
   $('#sign-out').on('submit', authEvents.onSignOut)
-  $('.square').on('click', gameApp.togglePlayer)
+  // Add event listener for our "Create Game" button
+  $('#create-game').on('submit', gameEvents.onCreateGame)
+  // Add event listener to update our board with every click of a game tile
+  $('.square').on('click', gameEvents.onUpdateBoard)
+  // Add event listener to toggle the player with every click of a game tile
+  $('.square').on('click', gameEngine.togglePlayer)
 })
