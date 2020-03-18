da66959e1f47723c4db90201c60bfa5e900e5249
rscottlundgren <rscottlundgren@gmail.com>
2020-03-17 23:43:35 -0400

    Refactor Code - 1)  Massive refactoring of code.

--- assets/scripts/game/engine.js
+++ assets/scripts/game/engine.js
@@ -1,75 +1,85 @@
 'use strict'
 
-// Import 'store.js' to access the two game objects contained there
 const store = require('../store')
-// Import 'ui.js' so we can update the page when success/failure occurs
 const ui = require('./ui')
+const gameAPI = require('./api')
 
-// Assigned 'staticBoard' variable for easy testing trough function action
 let staticBoard = store.game.cells
 
-// Declared an 'isWinner' function to evaluate the game board for a winner
-const isWinner = function (staticBoard) {
+const noBudgie = function () {
+  const gameOver = store.game.game.over
+  const cellText = $(event.target).text()
+  if (gameOver === true) {
+    ui.invalidMove()
+    return
+  } else if (cellText !== '') {
+    ui.invalidMove()
+    return
+  }
+}
 
-  if (store.game.cells[0] !== '' && store.game.cells[0] === store.game.cells[1] && store.game.cells[1] === store.game.cells[2] ) {
-    store.game.over = true
-    if (store.game.cells[0] === 'X') {
+const isWinner = function (staticBoard) {
+  if (staticBoard[0] !== '' && staticBoard[0] === staticBoard[1] && staticBoard[1] === staticBoard[2] ) {
+    store.game.game.over = true
+    if (staticBoard[0] === 'X') {
+      ui.winnerPlayerOneSuccess()
+      noBudgie()
+    } else {
+      ui.winnerFalconSuccess()
+      noBudgie()
+    }
+  }
+  if (staticBoard[3] !== '' && staticBoard[3] === staticBoard[4] && staticBoard[4] === staticBoard[5] ) {
+    store.game.game.over = true
+    if (staticBoard[3] === 'X') {
       ui.winnerPlayerOneSuccess()
     } else {
       ui.winnerFalconSuccess()
     }
   }
-  if (store.game.cells[3] !== '' && store.game.cells[3] === store.game.cells[4] && store.game.cells[4] === store.game.cells[5] ) {
-    store.game.over = true
-    if (store.game.cells[3] === 'X') {
+  if (staticBoard[6] !== '' && staticBoard[6] === staticBoard[7] && staticBoard[7] === staticBoard[8] ) {
+    store.game.game.over = true
+    if (staticBoard[6] === 'X') {
       ui.winnerPlayerOneSuccess()
     } else {
       ui.winnerFalconSuccess()
     }
   }
-  if (store.game.cells[6] !== '' && store.game.cells[6] === store.game.cells[7] && store.game.cells[7] === store.game.cells[8] ) {
-    store.game.over = true
-    if (store.game.cells[6] === 'X') {
+  if (staticBoard[0] !== '' && staticBoard[0] === staticBoard[3] && staticBoard[3] === staticBoard[6] ) {
+    store.game.game.over = true
+    if (staticBoard[0] === 'X') {
       ui.winnerPlayerOneSuccess()
     } else {
       ui.winnerFalconSuccess()
     }
   }
-  if (store.game.cells[0] !== '' && store.game.cells[0] === store.game.cells[3] && store.game.cells[3] === store.game.cells[6] ) {
-    store.game.over = true
-    if (store.game.cells[0] === 'X') {
+  if (staticBoard[1] !== '' && staticBoard[1] === staticBoard[4] && staticBoard[4] === staticBoard[7] ) {
+    store.game.game.over = true
+    if (staticBoard[1] === 'X') {
       ui.winnerPlayerOneSuccess()
     } else {
       ui.winnerFalconSuccess()
     }
   }
-  if (store.game.cells[1] !== '' && store.game.cells[1] === store.game.cells[4] && store.game.cells[4] === store.game.cells[7] ) {
-    store.game.over = true
-    if (store.game.cells[1] === 'X') {
+  if (staticBoard[2] !== '' && staticBoard[2] === staticBoard[5] && staticBoard[5] === staticBoard[8] ) {
+    store.game.game.over = true
+    if (staticBoard[2] === 'X') {
       ui.winnerPlayerOneSuccess()
     } else {
       ui.winnerFalconSuccess()
     }
   }
-  if (store.game.cells[2] !== '' && store.game.cells[2] === store.game.cells[5] && store.game.cells[5] === store.game.cells[8] ) {
-    store.game.over = true
-    if (store.game.cells[2] === 'X') {
+  if (staticBoard[0] !== '' && staticBoard[0] === staticBoard[4] && staticBoard[4] === staticBoard[8] ) {
+    store.game.game.over = true
+    if (staticBoard[0] === 'X') {
       ui.winnerPlayerOneSuccess()
     } else {
       ui.winnerFalconSuccess()
     }
   }
-  if (store.game.cells[0] !== '' && store.game.cells[0] === store.game.cells[4] && store.game.cells[4] === store.game.cells[8] ) {
-    store.game.over = true
-    if (store.game.cells[0] === 'X') {
-      ui.winnerPlayerOneSuccess()
-    } else {
-      ui.winnerFalconSuccess()
-    }
-  }
-  if (store.game.cells[2] !== '' && store.game.cells[2] === store.game.cells[4] && store.game.cells[4] === store.game.cells[6] ) {
-    store.game.over = true
-    if (store.game.cells[2] === 'X') {
+  if (staticBoard[2] !== '' && staticBoard[2] === staticBoard[4] && staticBoard[4] === staticBoard[6] ) {
+    store.game.game.over = true
+    if (staticBoard[2] === 'X') {
       ui.winnerPlayerOneSuccess()
     } else {
       ui.winnerFalconSuccess()
@@ -77,46 +87,52 @@ const isWinner = function (staticBoard) {
   }
 }
 
-const isTie = function (staticBoard) {
-  for (let i = 0; i < staticBoard.length; i++) {
-    if (store.game.cells[i] === '') return
+const togglePlayer = function (event) {
+  noBudgie()
+  const clickedTile = event.target.id
+  if (store.player === 'X') {
+    $(event.target).text(store.player)
+    $(event.target).addClass('player')
+    store.cell.index = clickedTile
+    store.cell.value = store.player
+    store.game.game.cells[clickedTile] = store.player
+    ui.playerOneMove()
+    store.player = 'O'
+  } else {
+    $(event.target).text(store.player)
+    $(event.target).addClass('falcon')
+    store.cell.index = clickedTile
+    store.cell.value = store.player
+    store.game.game.cells[clickedTile] = store.player
+    ui.falconMove()
+    store.player = 'X'
   }
-  if (store.game.over === false) {
+  isWinner(staticBoard)
+  isTie(staticBoard)
+  gameAPI.updateBoard({
+    'game': {
+      'cell': {
+        'index': clickedTile,
+        'value': store.cell.value
+      },
+        'over': store.game.game.over
+    }
+  })
+}
+
+const isTie = function (staticBoard) {
+  const gameOver = store.game.game.over
+  for (let i = 0; i < staticBoard.length; i++) {
+    if (store.game.game.cells[i] === '') return
+  }
+  if (store.game.game.over === false) {
     ui.gameDraw()
   }
 }
 
-const togglePlayer = function () {
-  const cellText = $(event.target).text()
-  if (store.game.over === true) return
-  if (cellText === 'X' || cellText === 'O') {
-    ui.invalidMove()
-    return
-  } else if (store.player === 'X') {
-    $(event.target).text(store.player)
-    store.position = event.target.id
-    store.cell.index = event.target.id
-    store.cell.value = store.player
-    store.game.cells[store.position] = store.player
-    ui.playerOneMove()
-    isWinner(staticBoard)
-    isTie(staticBoard)
-    store.player = 'O'
-  } else {
-    $(event.target).text(store.player)
-    store.position = event.target.id
-    store.cell.index = event.target.id
-    store.cell.value = store.player
-    store.game.cells[store.position] = store.player
-    ui.falconMove()
-    isWinner(staticBoard)
-    isTie(staticBoard)
-    store.player = 'X'
-  }
-}
-
 module.exports = {
+  noBudgie,
   togglePlayer,
-  isWinner,
-  isTie
+  isTie,
+  isWinner
 }
