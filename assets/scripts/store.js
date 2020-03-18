9ddac7ff8f274cc65f79fc0d4450556a4824dea1
rscottlundgren <rscottlundgren@gmail.com>
2020-03-17 23:30:24 -0400

    Remove Comments; Add Variable for Future Use - 1)  Removed some out-of-date comments. 2)  Added variable for future use.

--- assets/scripts/store.js
+++ assets/scripts/store.js
@@ -1,34 +1,34 @@
 'use strict'
 
-// Set default value of player to 'X'
 let player = 'X'
 
-// Create variable 'cell' to hold the key/value pairs that will be used for the
-// 'update-game.sh' curl script and ajax call
 const cell = {
-  // Key/Value pair created for the index that was changed in the object by the
-  // player's click that will be needed during ajax call to update the game as
-  // requested
   index: undefined,
-  // Key/Value pair for the value that was entered into the index created in an
-  // object that will be needed during ajax call to update the game as requested
   value: ''
 }
 
-// Create variable 'game' that will hold the key/value pairs that will be used
-// for both ajax calls and for other features of the game (i.e. determining win,
-// loss, and tie states, etc.)
+// const wins = [
+//   [0, 1, 2],
+//   [3, 4, 5],
+//   [6, 7, 8],
+//   [0, 3, 6],
+//   [1, 4, 7],
+//   [2, 5, 8],
+//   [0, 4, 8],
+//   [2, 4, 6]
+// ]
+
 const game = {
-  // Key/Value pair that will hold the static status of the entire board
-  // throughout game player
   cells: ['','','','','','','','',''],
-  // Key/Value pair that will hold the overall 'game state' for determination of
-  // whether the game has been won, lost, or tied
   over: false
 }
+
+const games = ['']
 
 module.exports = {
   player,
   cell,
-  game
+  game,
+  games
+  // wins
 }
