71c86d1cd7c7a3989d8e3075e6a3880dc92affce
rscottlundgren <rscottlundgren@gmail.com>
2020-03-25 10:54:11 -0400

    Update UI Calls In "auth.js" - 1)  Removed all references to ('#falcon-output'), changing them to the     appropriate console line (i.e. #console-line-one, #console-line-two,     #console-line-three). 2)  Commented out console.logs 3)  Massively overhauled "signInSuccess","signOutSuccess", and     "changePasswordSuccess" variables. 4)  Created and built out "onDeltaSubmitFacade" variable. 5)  Created and built out "onDeltaSubmitCheck" variable. 6)  Created and built out "onPasswordCheckYes" variable. 7)  Created and built out "onPasswordCheckNo" variable. 8)  Misc. small edits/touch-ups.

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -4,66 +4,169 @@ const store = require('../store')
 
 // Sign-Up User Interface
 const signUpSuccess = function (data) {
-  $('#falcon-output').text('Signed up successfully')
+  $('#console-line-one').text('Registration Successful.')
   $('#sign-up').trigger('reset')
-  $('.hide').show()
-  $('.show').hide()
-  console.log('signUpSuccess data is: ', data)
+  $('#sign-up').addClass('hide')
+  // console.log('signUpSuccess data is: ', data)
 }
 const signUpFailure = function (error) {
-  $('#falcon-output').text('Error on sign up')
+  $('#console-line-one').text('Registration unsuccessful. Confirm matching credentials.')
   $('#sign-up').trigger('reset')
-  console.log('signUpFailure error is: ', error)
+  // console.log('signUpFailure error is: ', error)
 }
 
 // Sign-In User Interface
 const signInSuccess = function (data) {
-  $('#falcon-output').text('Signed in successfully')
+  $('#console-line-one').text('Authentication Successful. Welcome, Professor.')
   $('#sign-in').trigger('reset')
-  $('.hide').show()
-  $('.show').hide()
-  console.log(data)
+  $('#sign-in').addClass('hide')
+  $('#game-stats').removeClass('hide')
+  $('#create-game').removeClass('hide')
+  $('#sign-out').removeClass('hide')
+  $('#gameboard').removeClass('hide')
+  $('#delta-form').removeClass('hide')
+  $('#delta-old').addClass('hide')
+  $('#delta-new').addClass('hide')
+  $('#delta-submit-facade').removeClass('hide')
+  $('#open-register-form').addClass('hide')
+  $('#open-authenticate-form').addClass('hide')
+  // console.log(data)
   store.user = data.user
-  console.log(store)
+  // console.log(store)
 }
 const signInFailure = function (error) {
-  $('#falcon-output').text('Error on sign in')
+  $('#console-line-one').text('Authentication unsuccessful. Please try again.')
   $('#sign-in').trigger('reset')
 }
 
 // Change Password User Interface
+const onDeltaSubmitFacade = function (event) {
+  event.preventDefault()
+  $('#console-line-one').text('')
+  $('#delta-submit-facade').addClass('hide')
+  $('#delta-submit-check').removeClass('hide')
+  $('#delta-old').removeClass('hide')
+  $('#delta-new').removeClass('hide')
+  $('#delta-old').removeClass('hide')
+  $('#delta-new').removeClass('hide')
+  $('#game-stats').addClass('hide')
+  $('#create-game').addClass('hide')
+  $('#sign-out').addClass('hide')
+}
+const onDeltaSubmitCheck = function (event) {
+  event.preventDefault()
+  $('#console-line-one').text('Have you used this password on another website?')
+  $('#console-line-two').text('')
+  $('#delta-submit-y').removeClass('hide')
+  $('#delta-submit-n').removeClass('hide')
+}
+const onPasswordCheckYes = function (event) {
+  event.preventDefault()
+  $('#delta-old').val('')
+  $('#delta-new').val('')
+  $('#console-line-one').text('Please create a unique password.')
+  $('#console-line-two').text('Do not reuse prior credentials.')
+  $('#delta-submit-y').addClass('hide')
+  $('#delta-submit-n').addClass('hide')
+  $('#delta-submit-check').removeClass('hide')
+  $('#delta-submit-final').addClass('hide')
+}
+const onPasswordCheckNo = function (event) {
+  event.preventDefault()
+  $('#console-line-one').text('Credential check passed.')
+  $('#console-line-two').text('You may reauthenticate, Professor.')
+  $('#delta-submit-check').addClass('hide')
+  $('#delta-submit-final').removeClass('hide')
+  $('#delta-submit-y').addClass('hide')
+  $('#delta-submit-n').addClass('hide')
+}
 const changePasswordSuccess = function (data) {
-  $('#falcon-output').text('Password changed successfully')
-  $('#change-password').trigger('reset')
+  $('#console-line-one').text('Credential successfully updated.')
+  $('#console-line-two').text('')
+  $('#delta-submit-final').addClass('hide')
+  $('#delta-submit-facade').removeClass('hide')
+  $('#delta-old').addClass('hide')
+  $('#delta-old').val('')
+  $('#delta-new').addClass('hide')
+  $('#delta-new').val('')
+  $('#game-stats').removeClass('hide')
+  $('#create-game').removeClass('hide')
+  $('#sign-out').removeClass('hide')
 }
 const changePasswordFailure = function (error) {
-  $('#falcon-output').text('Error on change password')
-  $('#change-password').trigger('reset')
+  event.preventDefault()
+  $('#console-line-one').text('Credential update unsuccessful. Please try again.')
+  $('#delta-old').val('')
+  $('#delta-new').val('')
 }
 
 // Sign-Out User Interface
 const signOutSuccess = function (data) {
-  $('#falcon-output').text('Signed out successfully')
-  $('.hide').hide()
-  $('.show').show()
-  $('.col-4.box.zero').css('border-color', 'black')
-  $('.col-4.box.zero').text('')
-  $('.col-4.box.one').css('border-color', 'black')
-  $('.col-4.box.one').text('')
-  $('.col-4.box.two').css('border-color', 'black')
-  $('.col-4.box.two').text('')
-  $('.col-4.box.three').css('border-color', 'black')
-  $('.col-4.box.three').text('')
-  $('.col-4.box.four').css('border-color', 'black')
-  $('.col-4.box.four').text('')
-  $('.col-4.box.five').css('border-color', 'black')
-  $('.col-4.box.five').text('')
-  $('.col-4.box.six').css('border-color', 'black')
-  $('.col-4.box.six').text('')
-  $('.col-4.box.seven').css('border-color', 'black')
-  $('.col-4.box.seven').text('')
-  $('.col-4.box.eight').css('border-color', 'black')
-  $('.col-4.box.eight').text('')
+  $('#console-line-one').text('--- Connection terminated. ---')
+  $('#console-line-two').text('')
+  $('#auth-successful').addClass('hide')
+  $('#auth-unsuccessful').addClass('hide')
+  $('#choose-opponent').addClass('hide')
+  $('#player-move').addClass('hide')
+  $('#O-to-0').addClass('hide')
+  $('#O-to-1').addClass('hide')
+  $('#O-to-2').addClass('hide')
+  $('#O-to-3').addClass('hide')
+  $('#O-to-4').addClass('hide')
+  $('#O-to-5').addClass('hide')
+  $('#O-to-6').addClass('hide')
+  $('#O-to-7').addClass('hide')
+  $('#O-to-8').addClass('hide')
+  $('#X-to-0').addClass('hide')
+  $('#X-to-1').addClass('hide')
+  $('#X-to-2').addClass('hide')
+  $('#X-to-3').addClass('hide')
+  $('#X-to-4').addClass('hide')
+  $('#X-to-5').addClass('hide')
+  $('#X-to-6').addClass('hide')
+  $('#X-to-7').addClass('hide')
+  $('#X-to-8').addClass('hide')
+  $('#invalid-move-gp').addClass('hide')
+  $('#you-won').addClass('hide')
+  $('#you-lost').addClass('hide')
+  $('#stalemate').addClass('hide')
+  $('#invalid-move-go').addClass('hide')
+  $('#play-game-yes').addClass('hide')
+  $('#open-register-form').addClass('hide')
+  $('#play-game-no').addClass('hide')
+  $('#open-authenticate-form').addClass('hide')
+  $('#unique-pw-q').addClass('hide')
+  $('#unique-pw-r').addClass('hide')
+  $('#auth-update-successful').addClass('hide')
+  $('#play-again').addClass('hide')
+  $('#delta-submit-y').addClass('hide')
+  $('#delta-submit-n').addClass('hide')
+  $('#play-again-y').addClass('hide')
+  $('#play-again-n').addClass('hide')
+  $('#sign-up').addClass('hide')
+  $('#sign-in').addClass('hide')
+  $('#game-stats').addClass('hide')
+  $('#delta-form').addClass('hide')
+  $('#delta-old').addClass('hide')
+  $('#delta-new').addClass('hide')
+  $('#delta-submit-facade').addClass('hide')
+  $('#delta-submit-check').addClass('hide')
+  $('#delta-submit-final').addClass('hide')
+  $('#create-game').addClass('hide')
+  $('#sign-out').addClass('hide')
+  $('#gameboard').addClass('hide')
+  $('#0').addClass('hide')
+  $('#1').addClass('hide')
+  $('#2').addClass('hide')
+  $('#3').addClass('hide')
+  $('#4').addClass('hide')
+  $('#5').addClass('hide')
+  $('#6').addClass('hide')
+  $('#7').addClass('hide')
+  $('#8').addClass('hide')
+  setTimeout(function () {
+    window.location.replace("https://www.linkedin.com/in/rscottlundgren/")
+  }, 2250)
 }
 const signOutFailure = function (error) {
   $('#falcon-output').text('Error on sign out')
@@ -74,6 +177,10 @@ module.exports = {
   signUpFailure,
   signInSuccess,
   signInFailure,
+  onDeltaSubmitFacade,
+  onDeltaSubmitCheck,
+  onPasswordCheckYes,
+  onPasswordCheckNo,
   changePasswordSuccess,
   changePasswordFailure,
   signOutSuccess,
